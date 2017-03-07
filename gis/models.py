6a7b891d421beb1f5375e0ac119660c6778e3483
Masha <laktio.laktio@gmail.com>
2017-03-07 09:33:06 +0200

    add commet

---
+++ gis/models.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class Articleas(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    user = models.ForeignKey(User)
+
